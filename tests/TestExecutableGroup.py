15cfd2cd742c5d6253bfa0d93370dbddc0fb2ef0
stefanoborini <stefanoborini@0ea1c711-7a27-4ec1-8190-1f350608a5f1>
2008-11-09 21:01:04 +0000

    added folder markers
    git-svn-id: https://chestnut.svn.sourceforge.net/svnroot/chestnut/trunk@49 0ea1c711-7a27-4ec1-8190-1f350608a5f1

--- tests/TestExecutableGroup.py
+++ tests/TestExecutableGroup.py
@@ -6,30 +6,30 @@ from Chestnut import ExecutableGroup
 from Chestnut import Executable
 
 class TestExecutableGroup(unittest.TestCase):
-    def testInitialization(self):
+    def testInitialization(self): # fold>>
         executable_group = ExecutableGroup.ExecutableGroup() 
         self.assertEqual(executable_group.__class__, ExecutableGroup.ExecutableGroup)
         self.assertEqual(executable_group.entryPoint(), None)
         self.assertEqual(executable_group.executableList(), [])
         self.assertEqual(executable_group.description(), None)
-
-    def testEntryPoint(self):
+        # <<fold
+    def testEntryPoint(self): # fold>>
         executable_group = ExecutableGroup.ExecutableGroup()
         executable_group.setEntryPoint("hello")
         self.assertEqual(executable_group.entryPoint(), "hello")
-
-    def testExecutableList(self):
+        # <<fold
+    def testExecutableList(self): # fold>>
         executable_group = ExecutableGroup.ExecutableGroup()
         executable = Executable.Executable()
         executable_group.addExecutable(executable)
         self.assertEqual(len(executable_group.executableList()), 1)
         self.assertEqual(executable_group.executableList()[0], executable)
-
-    def testDescription(self):
+        # <<fold
+    def testDescription(self): # fold>>
         executable_group = ExecutableGroup.ExecutableGroup()
         executable_group.setDescription("hello")
         self.assertEqual(executable_group.description(), "hello")
-
+        # <<fold
 
 if __name__ == '__main__':
     unittest.main()
