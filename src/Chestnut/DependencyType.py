0d122ab7fa6315e4a1a25b32e8aeef1647112734
stefanoborini <stefanoborini@0ea1c711-7a27-4ec1-8190-1f350608a5f1>
2009-03-29 21:03:31 +0000

    removed everything, we roll back to 2.0.0
    git-svn-id: https://chestnut.svn.sourceforge.net/svnroot/chestnut/trunk@57 0ea1c711-7a27-4ec1-8190-1f350608a5f1

--- src/Chestnut/DependencyType.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# @author Stefano Borini
-PACKAGED_EXECUTABLE="PACKAGED_EXECUTABLE"
-
-def validDependencyType(dependency_type):
-    return dependency_type in (PACKAGED_EXECUTABLE, )
-
-    
